e44d131eca03e6f6983e4238dc27f8ad43f89958
hrhasib <mailtohrhasib@gmail.com>
2022-01-27 12:05:58 +0600

    added availability section

--- src/Components/AvailabilitySection/index.jsx
+++ src/Components/AvailabilitySection/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
+import { Edit, EventAvailable } from '@mui/icons-material';
 
 const AvailabilitySection = ({ userData }) => {
     return (
@@ -9,7 +10,34 @@ const AvailabilitySection = ({ userData }) => {
             justifyContent: 'space-between',
             borderBottom: '0'
         }}>
-            Availability Section
+
+            <Grid container spacing={4} sx={{
+                marginRight: '30px'
+            }}>
+                <Grid item md={6} sm={6} xs={12} sx={{
+                    display: 'flex',
+                    alignItems: 'center'
+                }}>
+                    <EventAvailable sx={{ fontSize: '17px', marginRight: '8px' }} />
+                    Availability
+                </Grid>
+
+                <Grid item md={6} sm={6} xs={12} sx={{
+                    textAlign: 'right',
+                    fontSize: '14px'
+                }}>
+                    <Box component="p" sx={{
+                        fontWeight: '600',
+                        marginBottom: '5px'
+                    }}>
+                        {userData?.availability ? userData.availability : 'Full time'}
+                    </Box>
+                </Grid>
+
+
+            </Grid>
+
+            <Edit sx={{ color: '#CDCDCD', cursor: 'pointer' }} />
         </Box>
     );
 };
